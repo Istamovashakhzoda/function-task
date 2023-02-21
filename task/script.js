6f57e029542a5ffe4f30311ef1dfa910e561c293
Istamovashakhzoda <117287001+Istamovashakhzoda@users.noreply.github.com>
2023-02-21 19:36:11 +0300

    Add files via upload

---
+++ task/script.js
@@ -0,0 +1,231 @@
+//"use script"
+/*let a3=Number(prompt("a"));
+let b3=Number(prompt("b"));
+let c3=Number(prompt("c"));
+let d3=Number(prompt("d"));
+let e3=Number(prompt("e"));
+functionName(a3, b3,c3,d3,e3);
+function functionName(a,b,c,d,e){
+  let a3=(a**3);
+  console.log(a3);
+  let b3=(b**3);
+  console.log(b3);
+  let c3=(c**3);
+  console.log(c3);
+  let d3=(d**3);
+  console.log(d3);
+  let e3=(e**3);
+  console.log(e3);
+}*/
+
+//2-MASALA
+/*let number1=Number(prompt("a"));
+let number2=Number(prompt("b"));
+let number3=Number(prompt("c"));
+functionName(number1, number2, number3);
+function functionName(a,b,c){
+  console.log((a*b*c)**2);
+}*/
+
+//3-MASALA
+/*let a=Number(prompt("a"));
+let b=Number(prompt("b"));
+let c=Number(prompt("c"));
+let d=Number(prompt("d"));
+functionName(a, b, c, d);
+function functionName(a,b,c,d){
+  console.log((a*b)(a*c)(a*d)**2);
+
+  return 0;
+}*/
+
+
+//4-MASALA
+/*let number1=Number(prompt("premetri"));
+let number2=Number(prompt("yuzasi"));
+functionName(number1,number2);
+function functionName(a,b){
+let number1=(3*a);
+console.log(number1);
+let number2=(3/a);
+console.log(number2);
+
+return 0;
+
+}*/
+
+//5-MASALA
+/*let a=Number(prompt("x1"));
+let b=Number(prompt("y1"));
+let d=Number(prompt("x2"));
+let e=Number(prompt("y2"));
+RectPC(a,b,d,e);
+function RectPC(a,b,d,e){
+  let RectPC=((a+b)(a+d)(a*b)(a*));
+  console.log(RectPC);
+}*/
+
+//6-MASALA
+/*let a=Number(prompt("a"));
+let b=Number(prompt("b"));
+let c=Number(prompt("c"));
+DigitCountSum(a,b,c)
+function DigitCountSum(a,b,c){
+  let DigitCountSum=((a+b+c)*c);
+  console.log(DigitCountSum);
+}*/
+
+//7-MASALA
+/*let a=prompt("a")
+let b=prompt("b")
+let c=prompt("c")
+let d=a
+let e=b
+let f=c
+function numb(a,b,c){
+  a=e;
+  b=f
+  c=d
+  console.log(b,c,a);
+  return b,c,a
+}
+numb(a,b,c)*/
+
+
+//8-masala
+/*let k=Number(prompt("Kson"));
+let r=Number(prompt("Rson"));
+ NameNumber(k,r);
+ function NameNumber(Kson,Rson){
+  let i=(1<=r<=9);
+  console.log(i);
+ }*/
+
+ //10-MASALA
+ /*let number1=Number(prompt("A"));
+ let number2=Number(prompt("B"));
+ let number3=Number(prompt("C"));
+ let number4=Number(prompt("D"));
+ funcName(number1,number2,number3,number4);
+ function funcName(A,B,C,D){
+  let number1=(D);
+  console.log(number1);
+  let number2=(C);
+  console.log(number2);
+  let number3=(B);
+  console.log(number3);
+  let number4=(A);
+  console.log(number4);
+ }*/
+
+ //11-MASALA
+ /*let a=prompt("a");
+ let b=prompt("b");
+ let c=prompt("c");
+ let d=prompt("d");
+ //function(a,b,c,d)
+ //function numb(a,b,c,d){
+  if(a<b){
+    let e=("X- kichik-qiymat")
+    console.log(e);
+   } else if(a>b){
+    let f=("Y-katta-qiymat")
+    console.log(f);
+   }
+ //}*/
+
+
+ //16-MASALA
+ /*let a=prompt("a");
+ let b=prompt("b");
+ functionraqam(a,b);
+ function functionraqam(a,b){
+  if(a<0){
+    console.log(-1);
+  }else if(a>=!0){
+    console.log(1);
+  }else if(a==0){
+    console.log(0);
+  }
+ }*/
+
+//17-MASALA
+/*let x=prompt("a");
+let b=prompt("b");
+let c=prompt("c")
+functionNumb(x,b);
+function functionNumb(a,b){
+  let e=(a*x**2+b*x+c);
+  console.log(e);
+}*/
+
+
+//19-MASALA
+/*let r1=prompt("R1")
+let r2=prompt("R2")
+functionRings(r1,r2)
+function functionRings(R1,R2){
+  let S=p R**2;
+  console.log(S);
+  let Pi=(Pi=3.1415)
+  console.log(Pi);
+}*/
+
+
+ //20-MASALA
+ /*let a=prompt("a")
+ let b=prompt("b")
+ function numb(a,b){
+  let p=(p=2(a+b));
+  console.log(p);
+}*/
+
+//-------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"use script"
+let mainTitle=document.querySelector(".main__left--title");
+ let mainDesc=document.querySelector(".main__lest--desc");
+ let mainMore=document.querySelector(".main__more");
+
+ let mainRight=document.querySelector(".main__right");
+ let openBtn=document.querySelector(".main__btn");
+ let closeBtn=document.querySelector(".close__btn");
+  
+ /*mainTitle.textContent="voluptatem quam dolor, cumque ratione molestiae magni";
+ mainDesc.textContent="voluptatem quam dolor, cumque ratione molestiae magni";
+
+ console.log(mainTitle.textContent);
+
+ mainMore.style.backgroundColor="yellow";*/
+
+ openBtn.addEventListener("click" , function(){
+  aler("Hello");
+  mainRight.getElementsByClassName.backgroundcolor="red"
+ }
+
+ 
+ 
+
+
+
+
+
